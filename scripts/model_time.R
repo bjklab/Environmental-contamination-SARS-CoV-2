09aa2eae06bcecb391e42f85d22447229038e80c
Brendan Kelly <brendank@upenn.edu>
2021-08-10 19:10:15 -0400

    Examine inter-subject differences in temporal model.

--- scripts/model_time.R
+++ scripts/model_time.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(gt)
 library(tidybayes)
 library(brms)
+library(patchwork)
 
 set.seed(16)
 
@@ -741,7 +742,7 @@ m_mvbinom_scv2_time_fix_category_touch$data %>%
          high_touch = unique(high_touch)
   ) %>%
   filter(!(high_touch == TRUE & site_category == "Floor")) %>%
-  add_epred_draws(m_mvbinom_scv2_time_fix_category_touch) %>%
+  add_epred_draws(m_mvbinom_scv2_time_fix_category_touch, ndraws = 1000) %>%
   identity() -> m_mvbinom_scv2_time_fix_category_touch_fitted
 m_mvbinom_scv2_time_fix_category_touch_fitted
 
@@ -762,6 +763,8 @@ m_mvbinom_scv2_time_fix_category_touch_fitted %>%
   theme(strip.text = ggtext::element_markdown(color = "black", size = 8),
         axis.text.x = ggtext::element_markdown(color = "black"),
         axis.text.y = ggtext::element_markdown(color = "black"),
+        axis.title.x = ggtext::element_markdown(color = "black"),
+        axis.title.y = ggtext::element_markdown(color = "black"),
         legend.position = "top",
         #legend.background = element_rect(fill = "white", color = "black", size = 0.25),
         strip.background = element_blank()) -> p_scv2_mvbinomial_subject_covid_day_site_category_touch
@@ -920,17 +923,211 @@ hurdle_time_category_touch_loo_comparison %>%
 
 
 #' ############################
-#' MODELS with SUBJECT-SPECIFIC INTERCEPTS
-#' - outliers
+#' MODELS with SUBJECT-LEVEL RANDOM EFFECTS
+#' PSIS diagnositics: plot(loo(m_binom_scv2_distance_mix_category_touch))
+#' outliers analysis via random effects
 #' ############################
-plot(loo(m_binom_scv2_distance_mix_category_touch))
+
+
+#' ####################################################
+#' generative model for SARS-CoV-2 contamination ~ days from COVID diagnosis
+#' - elevated vs floor -- accounting for high-touch
+#' - binomial probability of detection
+#' ####################################################
+
+dat_long %>%
+  # exclude bathroom sites and nursing sites (different distance scheme)
+  filter(grepl("bathroom|sink|toilet|nurse",site_descriptor) == FALSE) %>%
+  mutate(scv2_detected = !is.na(copies_max),
+         site_category = case_when(grepl("floor",site_descriptor) == TRUE ~ "floor",
+                                   grepl("floor",site_descriptor) == FALSE ~ "elevated"),
+         high_touch = touch == "High") %>%
+  #count(site_descriptor, site_category, high_touch)
+  select(subject_room_id, redcap_id, subject_room_day, subject_covid_day, subject_hosp_day, subject_covid_day, swab_site, unit, site_category, touch, high_touch, distance, copies_max, scv2_detected) %>%
+  mutate(redcap_id = paste0("decon_subject_", redcap_id)) %>%
+  mutate(site_category = stringr::str_to_title(gsub("_"," ",site_category))) %>%
+  distinct() %>%
+  identity() -> dat
+
+dat
+
+
+#' get prior
+dat %>%
+  brms::get_prior(data = ., family = bernoulli,
+                  scv2_detected ~ (1 + subject_covid_day + site_category + high_touch | subject_room_id)
+  ) %>%
+  gt::gt()
+
+
+#' run binomial model
+# dat %>%
+#   select(scv2_detected, subject_covid_day, site_category, high_touch, subject_room_id) %>%
+#   brm(formula = scv2_detected ~ (1 + subject_covid_day + site_category + high_touch | subject_room_id),
+#       data = .,
+#       family = bernoulli,
+#       prior = c(set_prior(prior = "normal(0, 1)",class = "Intercept"), set_prior(prior = "normal(0,1)", class = "sd")),
+#       chains = 4,
+#       cores = 4,
+#       iter = 3000,
+#       warmup = 1000,
+#       control = list("adapt_delta" = 0.999, max_treedepth = 10),
+#       backend = "cmdstanr",
+#       seed = 16) -> m_mvbinom_scv2_time_category_touch_mix_subject
+# 
+# m_mvbinom_scv2_time_category_touch_mix_subject %>% write_rds(file = "./models/binomial/m_mvbinom_scv2_time_category_touch_mix_subject.rds.bz2", compress = "bz2")
+m_mvbinom_scv2_time_category_touch_mix_subject <- read_rds(file = "./models/binomial/m_mvbinom_scv2_time_category_touch_mix_subject.rds.bz2")
+
+m_mvbinom_scv2_time_category_touch_mix_subject
+rstan::check_hmc_diagnostics(m_mvbinom_scv2_time_category_touch_mix_subject$fit)
+m_mvbinom_scv2_time_category_touch_mix_subject %>% pp_check()
+
+m_mvbinom_scv2_time_category_touch_mix_subject %>%
+  posterior_summary() %>%
+  as_tibble(rownames = "param") %>%
+  gt::gt() %>%
+  gt::fmt_number(columns = 2:5, n_sigfig = 3)
+
+
+#' fitted
+m_mvbinom_scv2_time_category_touch_mix_subject$data %>%
+  as_tibble() %>%
+  expand(subject_covid_day = modelr::seq_range(subject_covid_day, n = 40),
+         site_category = unique(site_category),
+         high_touch = unique(high_touch),
+         subject_room_id = unique(subject_room_id)
+  ) %>%
+  filter(!(high_touch == TRUE & site_category == "Floor")) %>%
+  add_epred_draws(m_mvbinom_scv2_time_category_touch_mix_subject, ndraws = 1000) %>%
+  identity() -> m_mvbinom_scv2_time_category_touch_mix_subject_fitted
+m_mvbinom_scv2_time_category_touch_mix_subject_fitted
+
+
+m_mvbinom_scv2_time_category_touch_mix_subject_fitted %>%
+  mutate(high_touch = case_when(high_touch == TRUE ~ "High Touch",
+                                high_touch == FALSE ~ "Low Touch")) %>%
+  ggplot(aes(x = subject_covid_day, y = .epred)) +
+  #geom_point(data = m_mvbinom_scv2_time_category_touch_mix_subject$data, aes(x = subject_covid_day, y = scv2_detected), color = "grey", alpha = 0.5) +
+  stat_lineribbon() +
+  facet_wrap(facets = ~ site_category + high_touch, scales = "fixed") +
+  #facet_wrap(facets = ~ site_category) +
+  scale_fill_brewer(palette = "Reds") +
+  labs(x = "Days after COVID-19 Diagnosis",
+       y = "Probability of SARS-CoV-2 Detection by RT-PCR",
+       fill = "Posterior Credible Interval") +
+  theme_bw() +
+  theme(strip.text = ggtext::element_markdown(color = "black", size = 8),
+        axis.text.x = ggtext::element_markdown(color = "black"),
+        axis.text.y = ggtext::element_markdown(color = "black"),
+        axis.title.x = ggtext::element_markdown(color = "black"),
+        axis.title.y = ggtext::element_markdown(color = "black"),
+        legend.position = "top",
+        #legend.background = element_rect(fill = "white", color = "black", size = 0.25),
+        strip.background = element_blank()) -> p_scv2_mvbinomial_time_site_category_touch_mix_subject
+p_scv2_mvbinomial_time_site_category_touch_mix_subject
+
+
+# p_scv2_mvbinomial_time_site_category_touch_mix_subject %>%
+#   ggsave(filename = "./figs/p_scv2_mvbinomial_time_site_category_touch_mix_subject.pdf", height = 5, width = 8, units = "in")
+# p_scv2_mvbinomial_time_site_category_touch_mix_subject %>%
+#   ggsave(filename = "./figs/p_scv2_mvbinomial_time_site_category_touch_mix_subject.png", height = 5, width = 8, units = "in", dpi = 600)
+# p_scv2_mvbinomial_time_site_category_touch_mix_subject %>%
+#   ggsave(filename = "./figs/p_scv2_mvbinomial_time_site_category_touch_mix_subject.svg", height = 5, width = 8, units = "in")
+
+
+#' compare subjects
+m_mvbinom_scv2_time_category_touch_mix_subject_fitted %>%
+  group_by(subject_room_id, subject_covid_day, site_category, high_touch) %>%
+  summarise(.epred = median(.epred, na.rm = TRUE)) %>%
+  mutate(high_touch = case_when(high_touch == TRUE ~ "High Touch",
+                                high_touch == FALSE ~ "Low Touch")) %>%
+  ggplot(aes(x = subject_covid_day, y = .epred, group = subject_room_id, color = subject_room_id)) +
+  #geom_line(color = "black", alpha = 0.5) +
+  geom_line() +
+  facet_wrap(facets = ~ site_category + high_touch, scales = "fixed") +
+  colorspace::scale_color_discrete_sequential(palette = "YlGnBu") +
+  labs(x = "Days after COVID-19 Diagnosis",
+       y = "Probability of SARS-CoV-2 Detection by RT-PCR",
+       fill = "Posterior Credible Interval") +
+  theme_bw() +
+  theme(strip.text = ggtext::element_markdown(color = "black", size = 8),
+        axis.text.x = ggtext::element_markdown(color = "black"),
+        axis.text.y = ggtext::element_markdown(color = "black"),
+        axis.title.x = ggtext::element_markdown(color = "black"),
+        axis.title.y = ggtext::element_markdown(color = "black"),
+        legend.position = "none",
+        #legend.background = element_rect(fill = "white", color = "black", size = 0.25),
+        strip.background = element_blank()) -> p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines
+p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines
+
+
+# p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines %>%
+#   ggsave(filename = "./figs/p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines.pdf", height = 5, width = 8, units = "in")
+# p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines %>%
+#   ggsave(filename = "./figs/p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines.png", height = 5, width = 8, units = "in", dpi = 600)
+# p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines %>%
+#   ggsave(filename = "./figs/p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines.svg", height = 5, width = 8, units = "in")
+
+
+
+#' patchwork plot joining fixed-effects and random-subject-effects models
+
+((p_scv2_mvbinomial_subject_covid_day_site_category_touch + labs(y = "Probability of SARS-CoV-2<br>Detection by RT-PCR")) / 
+    (p_scv2_mvbinomial_time_site_category_touch_mix_subject_lines + labs(y = "Probability of SARS-CoV-2<br>Detection by RT-PCR"))) +
+  patchwork::plot_annotation(tag_levels = "A") -> p_time_models_combined
+p_time_models_combined
+
+
+# p_time_models_combined %>%
+#   ggsave(filename = "./figs/p_time_models_combined.pdf", height = 7, width = 8, units = "in")
+# p_time_models_combined %>%
+#   ggsave(filename = "./figs/p_time_models_combined.png", height = 7, width = 8, units = "in", dpi = 600)
+# p_time_models_combined %>%
+#   ggsave(filename = "./figs/p_time_models_combined.svg", height = 7, width = 8, units = "in")
+
+
+#' examine relationship between subjects' floors and high-touch elevated
+m_mvbinom_scv2_time_category_touch_mix_subject_fitted %>%
+  filter(subject_covid_day == 0) %>%
+  filter((site_category == "Elevated" & high_touch == TRUE) | site_category == "Floor") %>%
+  group_by(subject_room_id, subject_covid_day, site_category, high_touch) %>%
+  summarise(.epred = median(.epred, na.rm = TRUE)) %>%
+  ungroup() %>%
+  mutate(numeric_id = gsub("decon_","",subject_room_id)) %>%
+  select(subject_room_id, numeric_id, site_category, .epred) %>%
+  pivot_wider(id_cols = c(subject_room_id, numeric_id), names_from = site_category, values_from = .epred) %>%
+  left_join(summarise(group_by(dat_long, subject_room_id), total_study_day = min(total_study_day, na.rm = TRUE), subject_room_day = min(subject_room_day, na.rm = TRUE), unit = unit[1]), by = "subject_room_id") %>%
+  #mutate(numeric_id = replace(numeric_id, Floor <= 0.75 | Elevated <= 0.75, NA)) %>%
+  ggplot(data = ., aes(x = Floor, y = Elevated, color = subject_room_id, label = numeric_id)) +
+  #geom_point() +
+  geom_point(shape = 21, color = "black") +
+  #ggrepel::geom_text_repel(color = "black", max.overlaps = 50) +
+  coord_equal(xlim = c(0,1), ylim = c(0,1)) +
+  #colorspace::scale_color_discrete_sequential() +
+  labs(x = "Probability of SARS-CoV-2<br>Detection on Floor",
+       y = "Probability of SARS-CoV-2<br>Detection on High-Touch Elevated",
+       ) +
+  theme_bw() +
+  theme(strip.text = ggtext::element_markdown(color = "black", size = 8),
+        axis.text.x = ggtext::element_markdown(color = "black"),
+        axis.text.y = ggtext::element_markdown(color = "black"),
+        axis.title.x = ggtext::element_markdown(color = "black"),
+        axis.title.y = ggtext::element_markdown(color = "black"),
+        legend.position = "none",
+        #legend.background = element_rect(fill = "white", color = "black", size = 0.25),
+        strip.background = element_blank())
+    
+
+
+
+
+
+
 
 
 #' ############################
-#' TIME MODELS WITH DAYS POST HOSPITALIZATION (in addition to days post COVID-19)
-#' - needs to be adjusted for changing rooms
-#' - ACCOUNT FOR UNIT TRANSFERS
-#' - dat_long %>% select(redcap_id, unit) %>% distinct() %>% count(redcap_id) %>% filter(n > 1)
+#' TIME MODELS WITH DAYS POST ROOM OCCUPENCY (in addition to days post COVID-19)
+#' - evaluate the effect of changing rooms
 #' ############################
 
 
